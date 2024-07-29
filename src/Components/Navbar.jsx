9b30f49469ffd8c3ad85c3702eba0edf5a93924f
VasudevPavan07 <pavanvasudev07@gmail.com>
2024-07-29 10:50:53 +0530

    Added new features and fixed bugs

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,24 +3,23 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div  className="navbar  py-8 w-full  fixed z-[999] flex items-center  justify-between ">
+    <div   className="py-8 w-full  fixed z-[999] navbar  ">
       
         <div className="Name  text-white  poppins-medium text-2xl ">
           <h1 className="px-5">Vasudev Pavan</h1>
         </div>
-        <div className="links  flex gap-10 text-white">
+        <div className="links absolute top-6 right-4  navitems flex gap-10 text-white">
           {["Profile", "Projects", "Education", "Contact"].map(
             (item, index) => (
-              <a
+              <a  
                 key={index}
-                className={`text-lg  capitalize font-light py-2 px-5 rounded ${index=== 3 && "ml-32"}`}
-              >
+                className={ ` text-lg  capitalize font-light py-2 px-5 rounded ${index=== 3 && "ml-32"}`}
+                style={{ marginLeft: (index === 3 && window.innerWidth > 960) ? "35px" : "-18px" }}
+             >
                 {item}
               </a>
             )
           )}
-         
-
         </div>
        
       
